84c9801c00895ac54c297a3e47b3eabbc6a120c3
courange <courange.c@gmail.com>
2021-05-02 09:51:01 +0900

    v0.1.12

--- app/src/main.js
+++ app/src/main.js
@@ -33,8 +33,44 @@ if (process.platform === 'darwin') {
     })
 }
 
+//#region win用updater
+let win;
+
+function sendStatusToWindow(text) {
+    log.info(text);
+    win.webContents.send('message', text);
+}
+function createDefaultWindow() {
+    win = new BrowserWindow();
+    win.webContents.openDevTools();
+    win.on('closed', () => {
+        win = null;
+    });
+    win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+    return win;
+}
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+})
+
 autoUpdater.on('update-downloaded', ({ version, files, path, sha512, releaseName, releaseNotes, releaseDate }) => {
-    const detail = `${app.getName()} ${version} ${releaseDate}`
+    const detail = `${app.getName()} ${version} ${releaseDate}`;
+    const win = new BrowserWindow({ width: 800, height: 600 });
 
     dialog.showMessageBox(
         win, // new BrowserWindow
@@ -53,6 +89,22 @@ autoUpdater.on('update-downloaded', ({ version, files, path, sha512, releaseName
         }
     )
 });
+
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+});
+app.on('ready', function () {
+    // Create the Menu
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    createDefaultWindow();
+});
+app.on('window-all-closed', () => {
+    app.quit();
+});
+
+//#endregion
 
 async function createWindow() {
     try {
